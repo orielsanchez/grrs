dd6a010de0ddbbe52ea8b2fd270639a06370b091
Oriel Sanchez <orielbenitosanchez@gmail.com>
2023-12-24 15:49:17 -0800

    nicer error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use anyhow::{Context, Result};
 use clap::Parser;
+// Example: cargo run -- main ./src/main.rs
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -9,13 +11,18 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}", args.path.display()))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    // Remember: Last expression of any block in Rust is its return value.
+    // Customary to omit a needless return.
+    // return Ok(()); same as:
+    Ok(())
 }
